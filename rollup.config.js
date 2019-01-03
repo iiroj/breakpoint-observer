12c6a3056bb4904f2d3b4b4c913ac51329debaed
Iiro Jäppinen <iiro@jappinen.fi>
2019-01-03 19:43:34 +0200

    feat: upgrade to Rollup v1.0 and build umd bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,44 @@
-import typescript from "rollup-plugin-typescript2";
+import commonjs from 'rollup-plugin-commonjs';
 import minify from "rollup-plugin-babel-minify";
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from "rollup-plugin-typescript2";
 
 import pkg from "./package.json";
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-  input: "src/index.tsx",
+const external = [...Object.keys(pkg.peerDependencies)]
 
-  output: [
-    {
-      exports: 'named',
-      file: pkg.main,
-      format: "cjs"
+const plugins = [
+  typescript({
+    tsconfig: "tsconfig.build.json",
+    typescript: require("typescript")
+  }),
+  production && minify({ comments: false })
+]
+
+export default [
+  {
+    input: "src/index.tsx",
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" }
+    ],
+    external,
+    plugins
+  },
+  {
+    input: "src/index.tsx",
+    output: {
+      file: pkg.browser,
+      format: 'umd',
+      name: 'breakpointObserver'
     },
-    {
-      exports: 'named',
-      file: pkg.module,
-      format: "es"
-    }
-  ],
-
-  external: [...Object.keys(pkg.peerDependencies || {})],
-
-  plugins: [
-    typescript({
-      tsconfig: "tsconfig.build.json",
-      typescript: require("typescript")
-    }),
-    production && minify({
-      comments: false
-    })
-  ]
-};
+    external,
+    plugins: [
+      resolve(),
+      commonjs(),
+      ...plugins
+    ]
+  }
+];
